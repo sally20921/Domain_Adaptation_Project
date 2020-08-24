2683be6643c1a36ed93bc273f3311b6d6bc4f2dc
sally20921 <sally20921@github.com>
2020-08-24 02:23:21 +0000

    before 08/24

---
+++ D/logger.py
@@ -0,0 +1,8 @@
+import os
+import logging 
+from tqdm import tqdm
+from tensorboardX import SummaryWriter
+import tensorflow as tf
+from utils import get_dirname_from_args, get_now
+
+
---
+++ D/utils.py
@@ -0,0 +1,28 @@
+from contextlib import contextmanager
+from datetime import datetime
+
+import os
+import sys
+import json
+import pickle
+import re
+import six
+import numpy as np
+import torch 
+from config import log_keys
+
+def get_dirname_from_args(args):
+    dirname=''
+    for key in sorted(log_keys):
+        dirname +=  '_'
+        dirname += key
+        dirname += '_'
+        dirname += str(args[key])
+
+    return dirname[1:]
+
+def get_now():
+    now = datetime.now()
+    return now.strftime('%Y-%m-%d-%H-%M-%S')
+
+
